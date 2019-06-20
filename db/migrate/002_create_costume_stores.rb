1fd0dd9eb72bfc0af1818c8286c1b3bbd8b6e1f2
Margot Konig <margot.konig@blackrock.com>
2019-06-20 02:00:33 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,10 +5,11 @@ class CreateCostumeStores < ActiveRecord::Migration
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :size
-      t.string :image_url
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.string :in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
